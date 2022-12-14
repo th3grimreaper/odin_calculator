313d63b201c4796b65d0a495f2641bdc9bf30e1e
reaper <saip589@gmail.com>
2022-12-14 07:41:43 +0530

    add border and button functionality

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+let buttons = document.querySelectorAll('.op-button');
+let screen = document.querySelector('.typing-area');
+let allClear = document.querySelector('.clear');
+
+//show things on screen upon click
+let buttonsArr = Array.from(buttons);
+buttonsArr.forEach((btn) => {
+  btn.addEventListener('click', printOnScr);
+});
+
+//print button values on screen
+function printOnScr(event) {
+  // console.log(event.target.textContent);
+  screen.textContent += event.target.textContent;
+}
+
+//clear screen
+allClear.addEventListener('click', clearScreen);
+
+function clearScreen() {
+  screen.textContent = "";
+}
