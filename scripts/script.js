c016fc62758732731ad45208afeb14f31b690bc4
reaper <saip589@gmail.com>
2022-12-15 15:37:50 +0530

    add functionality to operator buttons

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,98 @@
-let buttons = document.querySelectorAll('.op-button');
+let buttons = document.querySelectorAll('.num');
+let operators = document.querySelectorAll('.op');
 let calcScreen = document.querySelector('.calculation');
 let screen = document.querySelector('.typing-area');
 let allClear = document.querySelector('.clear');
 let backspace = document.querySelector('.backspace');
-let addBtn = document.querySelector('.add');
+let equalOp = document.querySelector('.equalto');
 
 //global variables
-let sum;
+let operationResult, numOne, numTwo, op;
 
 //clear screen
 allClear.addEventListener('click', clearScreen);
 //backspace 
 backspace.addEventListener('click', delOneChar);
-//addition
-addBtn.addEventListener('click', addNum);
 
-//add two (or more) numbers
-function addNum() {
-  
+//operator buttons
+Array.from(operators).forEach((operator) => {
+  operator.addEventListener('click', operateFunc);
+})
+
+function operateFunc(e) {
+  op = e.target.textContent;
+  switch(op) {
+    case '+':
+      numOne = parseFloat(screen.textContent);
+      screen.textContent = "";
+      calcScreen.textContent = numOne + op;
+      break;
+    case '-':
+      numOne = parseFloat(screen.textContent);
+      screen.textContent = "";
+      calcScreen.textContent = numOne + op;
+      break;
+    case '*':
+      numOne = parseFloat(screen.textContent);
+      screen.textContent = "";
+      calcScreen.textContent = numOne + op;
+      break;
+    case '/':
+      numOne = parseFloat(screen.textContent);
+      screen.textContent = "";
+      calcScreen.textContent = numOne + op;
+      break;
+    default:
+      console.log("ERROR!");
+      break;
+  }
 }
+
+equalOp.addEventListener('click', () => {
+  numTwo = parseFloat(screen.textContent);
+  calcScreen.textContent += numTwo;
+  screen.textContent = operate(op, numOne, numTwo);
+  operationResult = screen.textContent;
+});
+
+//perform operations
+function operate(operator, first, second) {
+  switch (operator) {
+    case "+":
+      operationResult = add(first,second);
+      break;
+    case "-":
+      operationResult = subtract(first, second);
+      break;
+    case "*":
+      operationResult = multiply(first, second);
+      break;
+    case "/":
+      operationResult = divide(first, second);
+      break;
+    default:
+      console.log("enter a valid operand");
+      break;
+  }
+  return operationResult;
+}
+
+//operation functions
+let add = (one, two) => {
+  return one+two;
+};
+
+let subtract = (one, two) => {
+  return one-two;
+};
+
+let multiply = (one, two) => {
+  return one*two;
+};
+
+let divide = (one, two) => {
+  return one/two;
+};
 
 //show things on screen upon click
 let buttonsArr = Array.from(buttons);
@@ -29,16 +103,24 @@ buttonsArr.forEach((btn) => {
 //print button values on screen
 function printOnScr(event) {
   screen.textContent += event.target.textContent;
-  if(screen.textContent.length >=12) {
+  if(screen.textContent.length > 12) {
     screen.textContent = screen.textContent.substring(0, screen.textContent.length - 1);
   }
 }
 
 function clearScreen() {
+  operationResult = 0;
+  numOne = 0;
+  numTwo = 0;
   screen.textContent = "";
+  calcScreen.textContent = "";
 }
 
 function delOneChar() {
   screen.textContent = screen.textContent.substr(0, screen.textContent.length - 1);
+  if(screen.textContent === "") {
+    screen.textContent = calcScreen.textContent;
+    calcScreen.textContent =  "";
+    screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
+  }
 }
-
