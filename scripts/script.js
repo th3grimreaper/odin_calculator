5abfc7c5b1368b4293ffe0855e317c353081622d
reaper <saip589@gmail.com>
2023-01-09 11:22:15 +0530

    reformat script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ let equalOp = document.querySelector('.equalto');
 let decimalOp = document.querySelector('.decimal');
 
 //global variables
-let operationResult; 
+let operationResult;
 let numOne;
 let numTwo;
 let op;
@@ -16,77 +16,77 @@ let decimals;
 
 //add decimal to number
 decimalOp.addEventListener('click', (e) => {
-  if(decimals) {
-    screen.textContent += "";
+  if (decimals) {
+    screen.textContent += '';
   } else {
     decimals = e.target.textContent;
     screen.textContent += decimals;
   }
-})
+});
 
 //clear screen
 allClear.addEventListener('click', clearScreen);
-//backspace 
+//backspace
 backspace.addEventListener('click', delOneChar);
 
 //operator buttons
 Array.from(operators).forEach((operator) => {
   operator.addEventListener('click', operateFunc);
-})
+});
 
 function operateFunc(e) {
   if (op) {
     numTwo = parseFloat(screen.textContent);
     operationResult = operate(op, numOne, numTwo);
     numOne = operationResult;
-    screen.textContent = "";
+    screen.textContent = '';
     op = e.target.textContent;
     calcScreen.textContent = numOne + op;
-    decimals = "";
+    decimals = '';
   } else {
     op = e.target.textContent;
     numOne = parseFloat(screen.textContent);
-    screen.textContent = "";
+    screen.textContent = '';
     calcScreen.textContent = numOne + op;
-    decimals = "";
+    decimals = '';
   }
 }
 
 equalOp.addEventListener('click', () => {
-  if(!screen.textContent) {
-    screen.textContent = "";
+  if (!screen.textContent) {
+    screen.textContent = '';
   } else {
     numTwo = parseFloat(screen.textContent);
-    if(screen.textContent === operationResult) {
-      calcScreen.textContent += "";
+    if (screen.textContent === operationResult) {
+      calcScreen.textContent += '';
     } else {
       calcScreen.textContent += numTwo;
     }
     screen.textContent = operate(op, numOne, numTwo);
     operationResult = screen.textContent;
     numOne = 0;
-    op = "";
-    decimals = "";
+    op = '';
+    decimals = '';
   }
 });
 
 //perform operations
 function operate(operator, first, second) {
   switch (operator) {
-    case "+":
-      operationResult = add(first,second);
+    case '+':
+      operationResult = add(first, second);
       break;
-    case "-":
+    case '-':
       operationResult = subtract(first, second);
       break;
-    case "*":
+    case '*':
       operationResult = multiply(first, second);
       break;
-    case "/":
+    case '/':
       operationResult = divide(first, second);
       break;
     default:
-      console.log("enter a valid operator");
+      console.log('enter a valid operator');
       break;
   }
   return parseFloat(operationResult);
@@ -94,19 +94,19 @@ function operate(operator, first, second) {
 
 //operation functions
 let add = (one, two) => {
-  return one+two;
+  return one + two;
 };
 
 let subtract = (one, two) => {
-  return one-two;
+  return one - two;
 };
 
 let multiply = (one, two) => {
-  return one*two;
+  return one * two;
 };
 
 let divide = (one, two) => {
-  return one/two;
+  return one / two;
 };
 
 //show things on screen upon click
@@ -118,29 +118,29 @@ buttonsArr.forEach((btn) => {
 //print button values on screen
 function printOnScr(event) {
   screen.textContent += event.target.textContent;
-  if(screen.textContent.length > 12) {
+  if (screen.textContent.length > 12) {
     screen.textContent = screen.textContent.substring(0, screen.textContent.length - 1);
   }
 }
 
 function clearScreen() {
-  op = "";
+  op = '';
   operationResult = 0;
   numOne = 0;
   numTwo = 0;
-  screen.textContent = "";
-  calcScreen.textContent = "";
-  decimals = "";
+  screen.textContent = '';
+  calcScreen.textContent = '';
+  decimals = '';
 }
 
 function delOneChar() {
   screen.textContent = screen.textContent.substr(0, screen.textContent.length - 1);
-  if(screen.textContent === "") {
+  if (screen.textContent === '') {
     screen.textContent = calcScreen.textContent;
-    calcScreen.textContent =  "";
+    calcScreen.textContent = '';
     screen.textContent = screen.textContent.slice(0, screen.textContent.length - 1);
-    op = "";
+    op = '';
     numOne = screen.textContent;
-    decimals = "";
+    decimals = '';
   }
 }
